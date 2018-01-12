fe7dfab5601f214b4cc6cb03c183d065cc14723f
shaikferozgithub <shaikferozmail@gmail.com>
2018-01-12 17:41:32 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,5 +9,26 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
 
+    m = RandomForestClassifier() # random forest model
+    y = df.loc[:,'SalePrice'] # dependent variable
+    X = df.loc[:,df.columns != 'SalePrice'] # independent variables
 
+    rfe = RFE(m, n_features_to_select=None)# , verbose=1)
+    rfe = rfe.fit(X, y)
+    rank = rfe.ranking_ # Get list of rankings = 1 means the column is selected
+
+    # Extract column name based on ratings from RFE algo
+    # Store indices of columns that have rating == 1
+    feature_indices = []
+    for i in xrange(len(rank)):
+        if rank[i] == 1:
+            feature_indices += [i]
+#     print feature_indices
+    ind = df.iloc[:,feature_indices].columns # Index object
+    return ind.values.tolist() # return as list
+
+f = rf_rfe(data)
+print type(f)
+print f
